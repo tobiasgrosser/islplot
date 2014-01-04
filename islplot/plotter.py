be759de7f500de82489f416a472cc06c879e262c
Tobias Grosser <tobias@grosser.es>
2014-01-04 19:42:54 +0100

    [doc] Document plot_points function

--- islplot/plotter.py
+++ islplot/plotter.py
@@ -5,9 +5,16 @@ def _get_point_coordinates(point):
     return (int(point.get_coordinate_val(_islpy.dim_type.set, 0).get_num_si()),
             int(point.get_coordinate_val(_islpy.dim_type.set, 1).get_num_si()))
 
-def plot_points(islset, color="black", size=15):
+def plot_points(set_data, color="black", size=15):
+    """
+    Plot the individual points of a two dimensional isl set.
+
+    :param set_data: The islpy.Set to plot.
+    :param color: The color of the points.
+    :param size: The diameter of the points.
+    """
     points = []
-    islset.foreach_point(lambda x: points.append(_get_point_coordinates(x)))
+    set_data.foreach_point(lambda x: points.append(_get_point_coordinates(x)))
     dimX = [x[0] for x in points]
     dimY = [x[1] for x in points]
     _plt.plot(dimX, dimY, ".", markersize=size, color=color, lw=0)
