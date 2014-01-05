1664b300299651912f64e9d8c47fd653bf6d4829
Tobias Grosser <tobias@grosser.es>
2014-01-05 18:07:54 +0100

    Support different type of markers

--- islplot/plotter.py
+++ islplot/plotter.py
@@ -5,19 +5,20 @@ def _get_point_coordinates(point):
     return (int(point.get_coordinate_val(_islpy.dim_type.set, 0).get_num_si()),
             int(point.get_coordinate_val(_islpy.dim_type.set, 1).get_num_si()))
 
-def plot_set_points(set_data, color="black", size=15):
+def plot_set_points(set_data, color="black", size=10, marker="o"):
     """
     Plot the individual points of a two dimensional isl set.
 
     :param set_data: The islpy.Set to plot.
     :param color: The color of the points.
     :param size: The diameter of the points.
+    :param marker: The marker used to mark a point.
     """
     points = []
     set_data.foreach_point(lambda x: points.append(_get_point_coordinates(x)))
     dimX = [x[0] for x in points]
     dimY = [x[1] for x in points]
-    _plt.plot(dimX, dimY, ".", markersize=size, color=color, lw=0)
+    _plt.plot(dimX, dimY, marker, markersize=size, color=color, lw=0)
 
 def _plot_arrow(start, end, graph, *args, **kwargs):
     shrinkA = 10
