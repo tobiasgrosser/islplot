b9c0790130d1701394b1e0330757f73c8acef5b0
Tobias Grosser <tobias@grosser.es>
2014-01-05 18:59:19 +0100

    Move README.txt to README.rst

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from setuptools import setup
 
 setup(
-		name='islplot',
-		description='Python module to render isl integer sets and maps',
-		version='0.1',
-		author='Tobias Grosser',
-		packages=['islplot']
+    name='islplot',
+    description='Python module to render isl integer sets and maps',
+    version='0.1',
+    author='Tobias Grosser',
+    long_description=open('README.rst').read(),
+    packages=['islplot']
 )
