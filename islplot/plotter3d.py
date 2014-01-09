4985b2803a334a3966ced014339c7ad84c2d3188
Tobias Grosser <tobias@grosser.es>
2014-01-09 12:55:35 +0100

    3D: Detect WebGL support automatically

--- islplot/plotter3d.py
+++ islplot/plotter3d.py
@@ -53,7 +53,16 @@ def get_scene_start():
         var lastTime = 0;
 
         // Build a basic scene
-        var renderer = new THREE.WebGLRenderer({ antialiasing: true });
+        var renderer
+
+        if (Detector.webgl) {
+            console.log("WEBGL")
+            renderer = new THREE.WebGLRenderer({ antialiasing: true });
+        } else {
+            console.log("CANVAS")
+            renderer = new THREE.CanvasRenderer();
+        }
+
         renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.5);
 
         var camera = new THREE.PerspectiveCamera(
@@ -124,6 +133,7 @@ def get_js_includes():
     string = """ <script language="JavaScript">"""
     string += THREE_JS
     string += ORBIT_CONTROLS
+    string += THREE_JS_DETECTOR
     string += """</script>"""
 
     return string
@@ -37798,3 +37808,64 @@ THREE.OrbitControls = function ( object, domElement ) {
 THREE.OrbitControls.prototype = Object.create( THREE.EventDispatcher.prototype );
 """
 
+THREE_JS_DETECTOR = r"""
+/**
+ * @author alteredq / http://alteredqualia.com/
+ * @author mr.doob / http://mrdoob.com/
+ */
+
+var Detector = {
+
+	canvas: !! window.CanvasRenderingContext2D,
+	webgl: ( function () { try { var canvas = document.createElement( 'canvas' ); return !! window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ); } catch( e ) { return false; } } )(),
+	workers: !! window.Worker,
+	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+
+	getWebGLErrorMessage: function () {
+
+		var element = document.createElement( 'div' );
+		element.id = 'webgl-error-message';
+		element.style.fontFamily = 'monospace';
+		element.style.fontSize = '13px';
+		element.style.fontWeight = 'normal';
+		element.style.textAlign = 'center';
+		element.style.background = '#fff';
+		element.style.color = '#000';
+		element.style.padding = '1.5em';
+		element.style.width = '400px';
+		element.style.margin = '5em auto 0';
+
+		if ( ! this.webgl ) {
+
+			element.innerHTML = window.WebGLRenderingContext ? [
+				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' ) : [
+				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' );
+
+		}
+
+		return element;
+
+	},
+
+	addGetWebGLMessage: function ( parameters ) {
+
+		var parent, id, element;
+
+		parameters = parameters || {};
+
+		parent = parameters.parent !== undefined ? parameters.parent : document.body;
+		id = parameters.id !== undefined ? parameters.id : 'oldie';
+
+		element = Detector.getWebGLErrorMessage();
+		element.id = id;
+
+		parent.appendChild( element );
+
+	}
+
+};
+"""
